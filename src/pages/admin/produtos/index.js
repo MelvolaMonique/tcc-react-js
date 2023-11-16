4717a317b3c7d773dcf06cfe1da9bb7cdf17dbec
MelvolaMonique <moniqueevelyn600@gmail.com>
2023-11-16 09:50:04 -0300

    fazendo a agenda

--- src/pages/admin/produtos/index.js
+++ src/pages/admin/produtos/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import {useState} from 'react'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Time from './Time.js';
 
-export default function Produtos() {
+function Produtos() {
+  const [date, setDate] = useState(new Date())
+  const[ showTime, setShowTime] = useState(false)
+
+
     return (
-        <div><h1>Listagem</h1></div>
+<div className="produtos">
+   <h1 className="header">React Calendar</h1>
+   <div className="calendar-container">
+     <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)}/>
+   </div>
+
+   <div className="text-center">
+      Selecione uma data: {date.toDateString()}
+   </div>
+   {date.length > 0 ? (
+     <p>
+       <span>começar</span>
+       {date[0].toDateString()}
+       &nbsp;
+       &nbsp;
+       <span>fim</span> {date[1].toDateString()}
+     </p>
+   ) : (
+     <p>
+       <span>Selecione uma data final: </span>{' '} {date.toDateString()}
+     </p>
+   )}
+   <Time showTime={showTime} date={date}/>
+ </div>
+
     );
 }
+export default Produtos;
